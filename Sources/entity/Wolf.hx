76135db450f69f8237932743b3a3c26b297087cf
Daniel B <danielblaker@protonmail.com>
2020-08-23 11:49:57 +1000

    Seperate collider into getCollider - modular

--- Sources/entity/Wolf.hx
+++ Sources/entity/Wolf.hx
@@ -74,11 +74,14 @@ class Wolf extends Entity {
 
 		resolveCollisions(level.colliders);
     }
+    function getCollider() {
+        return Polygon.rectangle(position.x - (size.x * .5) + velocity.x, position.y - (size.y) + velocity.y, size.x, size.y, false);
+    }
     function resolveCollisions(geometry:Array<differ.shapes.Shape>) {
-		var collides = false;
+        var collides = false;
+        var collider = getCollider();
 		for (shape in geometry) {
-			var potentialCollision = shape.testPolygon(Polygon.rectangle(position.x - (size.x * .5) + velocity.x, position.y - (size.y) + velocity.y, size.x, size.y,
-				false));
+			var potentialCollision = shape.testPolygon(collider);
 			if (potentialCollision != null) {
                 collides = true;
                 if (Math.abs(potentialCollision.separationX) > .1) {
